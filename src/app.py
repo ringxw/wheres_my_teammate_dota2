08557b87a1a7336e3caa82dbb18eab4e8c151d48
njiang <njiang@Nans-MacBook-Air.local>
2016-01-26 21:37:07 -0500

    Reads the form and the checkbox

--- src/app.py
+++ src/app.py
@@ -1,15 +1,32 @@
-from flask import Flask
-from flask import render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, url_for, abort, redirect
 
 app = Flask(__name__)
 
+#Global Vars
+POSITIONS = ['carry', 'mid', 'off', 'support', 'hard_support']
+REGIONS = ['usw', 'use', 'china']
+LANGUAGES = ['English','Russian','Chinese']
+
 @app.route('/')
 def home():
-    return 'Framework setup'
-
-@app.route('/index')
-def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    #should return results html here
+    positions = ', '.join(_parse_check_box(POSITIONS))
+    regions = ', '.join(_parse_check_box(REGIONS))
+    languages = ', '.join(_parse_check_box(LANGUAGES))
+    my_results = 'Player {0}, mmr {1}, plays positions {2}, plays on regions {3}, speaks {4}, looking for a teammate!!'.format(request.form['username'], request.form['mmr'], positions, regions, languages)
+    return my_results
+
+def _parse_check_box(input_list):
+    results = []
+    for item in input_list:
+        if request.form.getlist(item):
+            results.append(item)
+    return results
 
 if __name__ == '__main__':
     app.run(debug=True)
