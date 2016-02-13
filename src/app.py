f47887a19fd9308c698572b14ff8b78f44c11058
chqu <qu.chengchuan@gmail.com>
2016-02-13 03:30:23 -0500

    added readonly property to username inputs. if use disabled, input do not get POSTed causing badrequest error

--- src/app.py
+++ src/app.py
@@ -17,8 +17,11 @@ POSITIONS = ['carry', 'mid', 'off', 'support', 'hard_support']
 REGIONS = ['usw', 'use', 'china']
 LANGUAGES = ['English','Russian','Chinese']
 
+
 @app.route('/')
 def home():
+    init_session()
+    flash('new session')
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
@@ -27,8 +30,27 @@ def search():
     positions = ', '.join(_parse_check_box(POSITIONS))
     regions = ', '.join(_parse_check_box(REGIONS))
     languages = ', '.join(_parse_check_box(LANGUAGES))
+    
     my_results = 'Player {0}, mmr {1}, plays positions {2}, plays on regions {3}, speaks {4}, looking for a teammate!!'.format(request.form['username'], request.form['mmr'], positions, regions, languages)
-    return my_results
+    if session['logged_in']:
+        session['redo_search'] = True
+    #flash new player infos
+    #concat player data into flash info
+    flash(my_results)
+    return render_template('index.html')
+
+@app.route('/redo_search', methods=['POST'])
+def redo_search():
+    #should return results html here
+    positions = ', '.join(_parse_check_box(POSITIONS))
+    regions = ', '.join(_parse_check_box(REGIONS))
+    languages = ', '.join(_parse_check_box(LANGUAGES))
+    my_results = 'REDO_SEARCH Player {0}, mmr {1}, plays positions {2}, plays on regions {3}, speaks {4}, looking for a teammate!!'.format(request.form['username'], request.form['mmr'], positions, regions, languages)
+    session['redo_search'] = True
+    #flash new player infos
+    #concat player data into flash info
+    flash(my_results)
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -42,16 +64,25 @@ def login():
             error = 'Invalid password'
         else:
             session['logged_in'] = True #seems important, that's how the page knows you are logged in.
+            session['logged_name'] = request.form['username']
+            session['redo_search'] = False
             flash('You were logged in')
-            return redirect(url_for('home'))
+            return render_template('index.html')
     return render_template('login.html', error=error)
 
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    init_session()
     flash('You were logged out')
-    return redirect(url_for('home'))
+    return render_template('index.html')
+
+def init_session():
+    session['logged_in'] = False
+    session['logged_name'] = '_no_user'
+    session['redo_search'] = False
+
+    return 1
 
 def _parse_check_box(input_list):
     results = []
